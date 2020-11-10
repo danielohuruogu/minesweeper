ced91634604374ff81dc9d4ed2cafb687b47742f
dohuruogu123 <47153027+dohuruogu123@users.noreply.github.com>
2020-11-10 15:15:49 +0000

    Updated with animations

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+
 ///////////////////////////////////////////**************** SETTING UP GAME AREA*****************///////////////////////////////////////
 
 var gameStarted = false;
@@ -6,41 +6,83 @@ var gameStarted = false;
 const welcomeMessage = document.querySelector(".subHeading")
 const gameBoard = document.querySelector(".board")
 const gameStats = document.querySelector(".gameStats")
+const gameArea = document.querySelector(".gameArea")
+//  const refreshButton = document.querySelector("button")
+
+const difBtns = document.querySelectorAll(".difBtn")
+
+////////// difficulty booleans
+
+var isEasy = false;
+var isMed = false;
+var isHard = false;
+
+var difficulty
+var width
+var squares = [];
 
 gameStart();
 
 function gameStart() {
     if (!gameStarted) {
-        document.addEventListener("keypress", function listener(e) {
-                if (e.defaultPrevented) {
-                    return;
-                }
-                if (e.key === "Enter"&&!gameStarted) {
-                    console.log("enter key pressed");
+        for (var difBtn of difBtns) {
+            difBtn.addEventListener("click", function listener(e) {
                     welcomeMessage.classList.add("not-visible")
-                    gameBoard.classList.remove("not-visible")
-                    gameStats.classList.remove("not-visible")
+                    gameArea.classList.remove("not-visible")
+                    setDifficulty(e)
                     createBoard();
-                }
+                })
                 gameStarted = true;
-        })
+            }
+        }
+}
+
+function setDifficulty(event) {
+    if (event.target.classList.contains("easyBtn")) {
+        isEasy = true;
+        width = 8;
+        difficulty = 0.15625
+        gameBoard.classList.add("easy")
+    }
+    if (event.target.classList.contains("medBtn")) {
+        isMed = true;
+        width = 16;
+        difficulty = 0.2;
+        gameBoard.classList.add("medium")
+    }
+    if (event.target.classList.contains("hardBtn")) {
+        isHard = true;
+        width = 25;
+        difficulty = 0.2;
+        gameBoard.classList.add("hard")
     }
 }
 
-// createBoard();
+// var opacity = 0; 
+// var intervalID = 0; 
+// window.onload = fadeIn; 
+// I don't want this. I want fadeIn to happen once I've clicked a difficulty
+  
+// function fadeIn() { 
+//    setInterval(show, 200); 
+// } 
 
-var width = 8;
-
-var squares = [];
+// function show() { 
+//     var body = document.getElementById("body"); 
+//     opacity = Number(window.getComputedStyle(body).getPropertyValue("opacity")); 
+//     if (opacity < 1) { 
+//         opacity = opacity + 0.1; 
+//         body.style.opacity = opacity 
+//     } else { 
+//         clearInterval(intervalID); 
+//     } 
+// } 
 
 var bombNumber = 0;
 
 const bombText = document.querySelector(".bombsRemaining")
 
 function createBoard() {
-    console.log("gameboard started");
-
-    var difficulty = 0.15625;
 
     // setting the squares up
     for (var i = 0; i < width * width; i++) {
@@ -59,7 +101,6 @@ function createBoard() {
         //normal click
         square.addEventListener("click", function(e) { //on a click action, run 
             squareClick(square) //run function passing through square
-            console.log(e.target.classList);
         })
 
         //right click
@@ -79,13 +120,13 @@ function createBoard() {
 
         if (squares[i].classList.contains("noBomb")) {
             // 1, NW
-            if (!isLeftEdge && i - (width + 1) > 0 && squares[i - (width + 1)].classList.contains("bomb"))
+            if (!isLeftEdge && i - (width + 1) > -1 && squares[i - (width + 1)].classList.contains("bomb"))
                 totalBombsAround++
             // 2, N
-            if (i - width > 0 && squares[i - width].classList.contains("bomb"))
+            if (i - width > -1 && squares[i - width].classList.contains("bomb"))
                 totalBombsAround++
             // 3, NE
-            if (!isRightEdge && i - width + 1 > 0 && squares[i - width + 1].classList.contains("bomb"))
+            if (!isRightEdge && i - width + 1 > -1 && squares[i - width + 1].classList.contains("bomb"))
                 totalBombsAround++
             // 4, W
             if (!isLeftEdge && squares[i - 1].classList.contains("bomb"))
@@ -117,15 +158,25 @@ function squareClick(square) {
     if (isGameOver) return //if the game's done, do nothing
     if (square.classList.contains("checked") || square.classList.contains("flag")) return //if the squares been checked or has a flag, do nothing
     if (square.classList.contains("bomb")) {
-        gameOver(square) //if the square has a bomb, run the game over function
+        gameOver(currentId) //if the square has a bomb, run the game over function
     } else { //if none of those
+        square.classList.add("checked") // add 'checked' once it's done
         let total = square.getAttribute("data") //get the 'data' attribute
-        if (total != 0) { //if it's not 0 (if there are bombs nearby)
+        if (total == 0) { //
+            square.classList.add("emptyCheckedSquare")
+        } else {
+            if (total == 1) square.classList.add("blue")
+            if (total == 2) square.classList.add("green")
+            if (total == 3) square.classList.add("red")
+            if (total == 4) square.classList.add("purple")
+            if (total == 5) square.classList.add("brown")
+            if (total == 6) square.classList.add("yellow")
+            if (total == 7) square.classList.add("turquoise")
+            if (total == 8) square.classList.add("pink")
             square.innerHTML = total //change the inner html to that total
             return
         }
-        square.classList.add("checked") //then add 'checked' once it's done
-        console.log("checked has been added");
+        
         checkNeighbour(currentId) //check the neighbour of the current square
     }
 }
@@ -191,7 +242,7 @@ var flags = 0;
 
 function addFlag(square) {
     if (isGameOver) return
-    if (!square.classList.contains("checked") && (flags < bombNumber)) {
+    if (!square.classList.contains("checked")) {
         if (!square.classList.contains("flag")) {
             square.classList.add("flag")
             square.innerHTML = "🚩"
@@ -201,9 +252,16 @@ function addFlag(square) {
             square.classList.remove("flag")
             square.innerHTML = ""
             flags--
+            checkforWin();
         }
     }
-    bombText.innerHTML = "Bombs remaining: " + (bombNumber-flags) ;
+    
+    bombsRemaining = bombNumber - flags;
+    bombText.innerHTML = "Bombs remaining: " + (bombsRemaining);
+    const warning = document.querySelector(".warning")
+    if (!isGameOver) {
+        warning.classList.toggle("not-visible", bombsRemaining > 0)
+    }
 }
 
 //////////////////////////**************** END OF GAME FUNCTIONS ************//////////////////////////////////////
@@ -212,17 +270,37 @@ const gameMessage = document.querySelector(".gameMessage");
 
 var isGameOver = false;
 
-function gameOver(square) {
-    console.log("Game over ☹");
+function gameOver(squareId) {
     const endMessage_Loss = document.createElement("h3")
     endMessage_Loss.innerHTML = "Game over"
     gameMessage.appendChild(endMessage_Loss)
     isGameOver = true;
 
     squares.forEach(square => {
-        if (square.classList.contains("bomb")) {
-            square.innerHTML = "💣"
-        }
+        setTimeout(() => {
+            document.getElementById(squareId).classList.add("wrongSquare")
+            if (square.classList.contains("bomb")) {
+                square.innerHTML = "💣"
+            }
+        }, 50)
+        setTimeout(() => {
+            let bombCount = square.getAttribute("data")
+                if (!square.classList.contains("flag") && !square.classList.contains("bomb")) {
+                    if (bombCount == 0) {
+                        square.classList.add("emptyCheckedSquare")
+                    } else {
+                        if (bombCount == 1) square.classList.add("blue")
+                        if (bombCount == 2) square.classList.add("green")
+                        if (bombCount == 3) square.classList.add("red")
+                        if (bombCount == 4) square.classList.add("purple")
+                        if (bombCount == 5) square.classList.add("brown")
+                        if (bombCount == 6) square.classList.add("yellow")
+                        if (bombCount == 7) square.classList.add("turquoise")
+                        if (bombCount == 8) square.classList.add("pink")
+                        square.innerHTML = bombCount;
+                    }
+                }
+        }, 250)
     })
 }
 
@@ -234,12 +312,31 @@ function checkforWin() {
             matches++
         }
     }
+
     if (matches === bombNumber) {
         const endMessage_Win = document.createElement("h3");
         endMessage_Win.innerHTML = "Found all the bombs. You win"
         gameMessage.appendChild(endMessage_Win)
         isGameOver = true;
+        squares.forEach(square => {
+            setTimeout(() => {
+                let bombCount = square.getAttribute("data")
+                if (!square.classList.contains("flag") && !square.classList.contains("bomb")) {
+                    if (bombCount == 0) {
+                        square.classList.add("emptyCheckedSquare")
+                    } else {
+                        if (bombCount == 1) square.classList.add("blue")
+                        if (bombCount == 2) square.classList.add("green")
+                        if (bombCount == 3) square.classList.add("red")
+                        if (bombCount == 4) square.classList.add("purple")
+                        if (bombCount == 5) square.classList.add("brown")
+                        if (bombCount == 6) square.classList.add("yellow")
+                        if (bombCount == 7) square.classList.add("turquoise")
+                        if (bombCount == 8) square.classList.add("pink")
+                        square.innerHTML = bombCount;
+                    }
+                }
+            }, 200)
+        })
     }
 }
-
-})
